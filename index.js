1ed04fc584d3b8799067d58c4ef9d9d6f63152fc
janekigenyi <janekigenyi@gmail.com>
2022-09-10 10:55:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = (arrayDrivers) => {
-  return arrayDrivers.slice(4,2,7);
-}
+const returnFirstTwoDrivers = (drivers) => {
+  return [...drivers].slice(0, 2);
+};
 
-const returnLastTwoDrivers = (arrayDrivers) => {
-  return arrayDrivers.slice(-2);
-}
+const returnLastTwoDrivers = (drivers) => {
+  return [...drivers].slice(-2);
+};
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const createFareMultiplier = (multiple) => {
-  let callback = (num) => num*multiple;  
-  return callback;
-}
+const createFareMultiplier = (myValue) => {
+  return (fare) => {
+    // Fare Multiplication
+    return myValue * fare;
+  };
+};
 
-const fareDoubler = (num) => {
-  let callback = createFareMultiplier(2); 
-  return callback(num); 
-}
+let fareDoubler = createFareMultiplier(2);
+let fareTripler = createFareMultiplier(3);
 
-const fareTripler = (num) => {
-  let callback = createFareMultiplier(3);
-  return callback(num);
-}
-
-const selectDifferentDrivers = (arrayOfDrivers, fn) => {
-  if (fn === returnFirstTwoDrivers){
-      return returnFirstTwoDrivers(arrayOfDrivers);
-  } else if (fn === returnLastTwoDrivers){
-      return returnLastTwoDrivers(arrayOfDrivers);
-  } else {
-      return "invalid"
-  }
-}
-  
+const selectDifferentDrivers = (arrayOfDrivers, driversToReturn) => {
+  return driversToReturn(arrayOfDrivers);
+};
